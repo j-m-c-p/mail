9a58b13ea2fada6cab35a365e05e8cce810df762
j-m-c-p <jhonatancubi@gmail.com>
2017-05-09 16:59:51 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,21 +1,20 @@
 <?php 
-
+        //Variables que reciben la informaciÃÂÃÂÃÂÃÂ³n de el formulario.
 	$para=$_GET['para'];
-	$g=$_GET['g'];
 	$edad=$_GET['edad'];
 	$asunto=$_GET['asunto'];
 	$mensaje=$_GET['mensaje'];
 
 	
 	$html = "<body>";
+        //Mensaje
 	$html .= $mensaje;
 	$html .= "<br>";
-	$html .= "<img src='http://www.approckola.com/pruebasena/moreno_harley/imagen.jpg'>";
+	$html .= "<img src='https://diagnostico.000webhostapp.com/mail/imagen.jpg'>";
 	$html .= "<br>";
-	$html .= "<a href='http://www.approckola.com/pruebasena/moreno_harley/recepcion.php?correo=$para&edad=$edad'>Entrar</a>";
+	$html .= "<a href='https://diagnostico.000webhostapp.com/mail/recepcion.php?correo=$para&edad=$edad'>Entrar</a>";
 	$html .= "</body>";
-	
-
+	// Para enviar un correo HTML, debe establecerse la cabecera Content-type
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
@@ -24,7 +23,8 @@
 	
 
 
-	echo"Enviando correo a ". $para;
+	echo"Correo enviado a ". $para ."<br>" . "Por favor ingresa a tu correo";
+        // Enviarlo
 	mail($para, $asunto, $html, $headers);
 
  ?>
